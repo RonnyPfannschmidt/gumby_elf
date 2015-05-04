bfa59d9431ffd5e70b7691745c7c8217609b975d
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2015-05-04 11:19:24 +0200

    remove unused code

--- src/lowlevel.py
+++ src/lowlevel.py
@@ -6,29 +6,6 @@ import pkgutil
 
 import zipfile
 
-from .metadata import EntryPoints, WheelInfo
 
 
-
-def metadata_11(spec, version):
-    # XXX: better pathhandling
-    # XXX: incomplete
-    result = [
-        'Name: $(name)\n' % spec.data,
-        'Version: ' + version + '\n',
-    ]
-    return ''.join(result)
-
-
-def entrypoints_11(spec):
-    ep = EntryPoints.from_spec_dict(spec)
-    return ep.to_11_metadata()
-
-
-def install_extra_requires(spec, requested_extras):
-    extras = spec.get('dependenciesExtra', {})
-    required = []
-    for extra in requested_extras:
-        required.extend(extras[extra])
-    return subprocess.call(['pip', 'install', '-q'] + required)
 
