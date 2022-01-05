fabf0b46117768f9643ef47f6de44c50c75de237
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2022-01-05 20:11:18 +0100

    fix tests

--- testing/test_metadata.py
+++ testing/test_metadata.py
@@ -1,29 +1,28 @@
-from gumby_elf import metadata
+from gumby_elf import _metadata as metadata
 
 
 WHEEL_DEFAULT_META = """\
 Wheel-Version: 1.0
 Generator: gumby_elf pre alpha
 Root-Is-Purelib: true
-Tag: py27-none-any
 Tag: py3-none-any\
 """
 
 
 def test_entrypoints():
-    ep = metadata.EntryPoints.from_spec_dict(
-        {
+    class FakeMetadata:
+        gui_scripts = scripts = {}
+        entrypoints = {
             "pytest": {
                 "a": "b",
             }
         }
-    )
 
-    metadata_11 = ep.to_11_metadata()
-    assert metadata_11 == ("[pytest]\n" "a = b")
+    metadata_11 = metadata.entrypoints_txt(FakeMetadata)
+    assert metadata_11 == b"[pytest]\na = b\n\n"
 
 
 def test_wheelinfo():
-    wi = metadata.WheelInfo.default()
+    wi = metadata.get_wheel_info()
     print(wi)
-    assert str(wi) == WHEEL_DEFAULT_META
+    assert wi.decode() == WHEEL_DEFAULT_META
